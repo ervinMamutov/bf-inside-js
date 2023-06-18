983abdcf91728594e1e052c376c88771441d2748
ervinMamutov <ervin.news@gmail.com>
2023-06-18 23:51:50 +0200

    finish 2:2:3:3

--- 2-control-flow/2-integrate/3-numbers/exercises/3-repeat-string/1-blanks.js
+++ 2-control-flow/2-integrate/3-numbers/exercises/3-repeat-string/1-blanks.js
@@ -1,15 +1,13 @@
-// #todo
-
 'use strict';
 
 let userInput = '';
 let repetitions = NaN;
-while (_) {
+while (!userInput) {
   /* gather a string from the user */
   userInput = prompt('enter a phrase to repeat:');
 
   /* make sure the user input something */
-  if (_) {
+  if (userInput === null || userInput === '') {
     alert('nope, enter something');
     continue;
   }
@@ -21,25 +19,28 @@ while (_) {
   repetitions = Number(repetitionsInput);
 
   /* make sure the user input a valid number */
-  if (_) {
-    alert('"' + repetitionsInput + '" is not a number');
+  if (isNaN(repetitions)) {
+    alert(`${repetitionsInput} is not a number`);
     continue;
   }
 
   /* ask the user to confirm their input */
   const confirmMessage =
-    'is this correct?\n\n' + '- "' + userInput + '"\n' + '- ' + repetitions;
+    `is this correct?\n\n - ${userInput} + \n - ${repetitions}`;
   const confirmation = confirm(confirmMessage);
   if (confirmation) {
     break;
+  } else {
+    userInput = '';
+    continue;
   }
 }
 
 let repeatedInput = '';
 
 /* repeat the user input `repetitions` number of times */
-for (_; _; _) {
-  repeatedInput = _;
+for (let i = 0; i < repetitions; i++) {
+  repeatedInput += userInput;
 }
 
 const finalMessage = `"${userInput}" -> "${repeatedInput}"`;
