bef07af09ab67bf02d80b76eb5c55aba5007228f
ervinMamutov <ervin.news@gmail.com>
2023-06-21 15:47:09 +0200

    finish 2:3

--- 2-control-flow/3-dom-ui/reverse-string/script.js
+++ 2-control-flow/3-dom-ui/reverse-string/script.js
@@ -1,11 +1,18 @@
 import { readString, display } from '../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById('reverse-it').addEventListener('click', () => {
   debugger;
 
   // read user text
+  const userText = readString('user-text');
 
   // use a for loop that counts down (i--) to reverse the input
+  let result = '';
+
+  for (const char of userText) {
+    result = char + result;
+  }
 
   // display the reversed string
+  display('reversed-output', result);
 });
--- 2-control-flow/3-dom-ui/upside-down-pyramid/script.js
+++ 2-control-flow/3-dom-ui/upside-down-pyramid/script.js
@@ -1,15 +1,23 @@
 import { readString, display } from '../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById('pyramid-it').addEventListener('click', () => {
   debugger;
 
   // read user text
+  const pyramidTo = readString('to-pyramid');
 
   // create the pyramid
 
-  for (_; _; _) {
-    for (_; _; _) {}
+  let result = '';
+
+  for (let i = 0; i < pyramidTo.length; i++) {
+    result += pyramidTo[i];
+    for (let j = i + 1; j < pyramidTo.length; j++) {
+      result += pyramidTo[j];
+    }
+    result += '\n';
   }
 
   // display the pyramid
+  display('pyramided', result);
 });
