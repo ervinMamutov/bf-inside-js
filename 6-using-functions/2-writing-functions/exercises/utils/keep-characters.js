f611b776d1dfab40c35a0d9343fb524d8c2dc248
ervinMamutov <ervin.news@gmail.com>
2023-06-23 21:19:20 +0200

    finish6:2

--- 6-using-functions/2-writing-functions/exercises/utils/keep-characters.js
+++ 6-using-functions/2-writing-functions/exercises/utils/keep-characters.js
@@ -1,8 +1,32 @@
 /**
  * Returns a new string with only the given characters left in it.
  *
- * @param {_} [_=_] - Some text to remove characters from.
- * @param {_} [_=_] - The characters to keep.
- * @returns {_} Text with only the given characters still in it.
+ * @param {string} [text= ''] - Some text to remove characters from.
+ * @param {string} [characters= ''] - The characters to keep.
+ * @returns {string} Text with only the given characters still in it.
  */
-export const keepCharacters = () => {};
+
+  export const keepCharacters = (text = '', characters = '') => {
+    
+    const isLength = text.length === characters.length;
+    const isCharLowerCase = text.toLowerCase() === characters.toLowerCase();
+    const isCharInclude = text === characters;
+    
+    let result = '';
+    
+    if(isLength && !isCharLowerCase) return text;
+    if(isCharLowerCase && !isCharInclude && isLength) return '';
+    if (text === '' || characters === '') return '';
+
+    for (const char of characters) {
+      for (const textChar of text) {
+        if (char === textChar) {
+          result +=char;
+        } else {
+          result;
+        }
+      }
+    } 
+    return result;
+  };
+  
