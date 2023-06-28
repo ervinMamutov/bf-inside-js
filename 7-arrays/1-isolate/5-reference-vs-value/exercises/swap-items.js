0ce2e50f4f8119c23bf9e1b9301ecf60860a84b1
ervinMamutov <ervin.news@gmail.com>
2023-06-28 15:29:38 +0200

    finish 7:1:5

--- 7-arrays/1-isolate/5-reference-vs-value/exercises/swap-items.js
+++ 7-arrays/1-isolate/5-reference-vs-value/exercises/swap-items.js
@@ -12,6 +12,28 @@ let temp;
 
 // -- swap values (write this code) --
 
+temp = arr1;
+for (let i = 0; i < temp.length; i++){
+  if(typeof temp[i] !== 'number') {
+    arr2.push(temp[i]);
+    arr1.splice(i, 1);
+  }
+};
+
+arr2.sort();
+
+temp = arr2;
+for (let i = 0; i < temp.length; i++) {
+  if(typeof temp[i] !== 'string') {
+    arr1.push(temp[i]);
+    arr2.splice(i, 1);
+  }
+}
+
+arr1.sort();
+
+
+
 // -- assert values (this is correct) --
 console.assert(deepCompare(arr1, [1, 2, 3, 4]), 'array 1');
 console.assert(deepCompare(arr2, ['a', 'b', 'c']), 'array 2');
