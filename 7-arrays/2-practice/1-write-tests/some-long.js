cfc8a1e74aed2b31d8246aae110ce5329bc9fa7b
ervinMamutov <ervin.news@gmail.com>
2023-06-28 20:57:59 +0200

    finish 7:2

--- 7-arrays/2-practice/1-write-tests/some-long.js
+++ 7-arrays/2-practice/1-write-tests/some-long.js
@@ -1,5 +1,8 @@
 /**
- *
+ * checks the string length of the array and returns true or false depending on the number of long strings
+ * @param {string} [string=[]] origin array
+ * @param {number} [long=5] the number of long strings
+ * @returns {boolean} return true or false depending on the number of long string  
  */
 export const someLong = (strings = [], long = 5) => {
   let someAreLong = false;
--- 7-arrays/2-practice/2-fix-bugs/reverse-array.js
+++ 7-arrays/2-practice/2-fix-bugs/reverse-array.js
@@ -1,9 +1,11 @@
 // #todo
 
 /**
- *
+ * reverseArray: reverses an array of anything
+ * @param {string} [arr=[]] origin array
+ * @returns {string} reversed array
  */
 export const reverseArray = (arr = []) => {
-  const backwards = arr.reverse();
-  return backwards;
+  const backwards = [...arr];
+  return backwards.reverse();
 };
